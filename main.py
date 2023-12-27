bf2b226b9b4ba176deb134590b384590bc03f9fa
YoNoSoyMarinero1 <1milosmarinkovic1@gmail.com>
2023-12-27 18:12:53 +0100

    Add SPARQ filtering

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import cv2
 import numpy as np
-from onto_model.onto_model import IndividualGenerator
+from onto_model.onto_model import IndividualGenerator, SPARQL
 
 
-def image_statistics(image_mask):
-    flat_image = image_mask[:,:,0].flatten()
+def image_statistics(image_mask: np.array) -> zip:
+    flat_image: np.array = image_mask[:,:,0].flatten()
     unique_values, counts = np.unique(flat_image, return_counts=True)
     counts = [round(el, 2) for el in 100*counts/flat_image.shape[0]]
     return zip(unique_values, counts)
 
-def create_individuals(x, y, image_statistics, image_name):
+def create_terrene_individuals(image: np.array, image_name: str, x: int, y: int) -> None:
+    image_statistics: zip = image_statistics(image)
     for key, value in image_statistics:
         IndividualGenerator.create_terrene_individual(x, y, value, key, image_name)
+
+
--- onto_model/onto_model.py
+++ onto_model/onto_model.py
@@ -1,6 +1,5 @@
 import owlready2 as owl
 import os
-import cv2
 
 owl.onto_path.append(r"C:\Users\1milo\Desktop\EcoOntology\envo.owl")
 onto = owl.get_ontology(r"C:\Users\1milo\Desktop\EcoOntology\envo.owl")
@@ -37,10 +36,10 @@ with onto:
         pass
     class hasX(owl.DataProperty, owl.FunctionalProperty):
         domain = [onto.Terrene]
-        range = [float]
+        range = [int]
     class hasY(owl.DataProperty, owl.FunctionalProperty):
         domain = [onto.Terrene]
-        range = [float]
+        range = [int]
     class hasPercentage(owl.DataProperty, owl.FunctionalProperty):
         domain = [onto.Terrene]
         range = [float]
@@ -59,8 +58,41 @@ with onto:
 
 onto.save()
 
+class SPARQL:
+
+    prefix: str = """          
+            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+            PREFIX eco: <C:/Users/1milo/Desktop/EcoOntology/envo.owl#>"""
+    
+    @classmethod
+    def get_individuals_by_condition(cls, 
+                                     class_name: str = "Terrene",
+                                     x1: int = 0,
+                                     y1: int = 0,
+                                     x2: int = 1e6,
+                                     y2: int = 1e6,
+                                     min_percentage: float = 0,
+                                     max_percentage: float = 100) -> list:
+        query:str = f"""
+        {cls.prefix}
+        SELECT ?class ?x ?y ?p ?image
+        WHERE {{
+            ?class rdf:type/rdfs:subClassOf* eco:{class_name} .
+            ?class eco:hasX ?x .
+            ?class eco:hasY ?y .
+            ?class eco:hasPercentage ?p .
+            ?class eco:hasImage ?image.
+            FILTER (?x >= {x1} && ?x <= {x2} && ?y >= {y1} && ?y <= {y2} && ?p >= {min_percentage} && ?p <= {max_percentage})
+        }}
+    """
+        return list(owl.default_world.sparql(query))
+
+
 class IndividualGenerator():
-    class_map = {
+    class_map: dict = {
         0: (Empty, "empty"),
         1: (Willow,"willow"),
         2: (Poplar,"poplar"),
@@ -77,10 +109,10 @@ class IndividualGenerator():
     }
 
     @classmethod
-    def create_terrene_individual(cls, x, y, percentage, class_label, image_name):
-        individual_class = cls.class_map[class_label]
-        individual_name = f"{image_name}_{x}_{y}_{individual_class[1]}"
-        individual = individual_class[0](individual_name)
+    def create_terrene_individual(cls, x: int, y: int, percentage: float, class_label: int, image_name: str) -> None:
+        individual_class: object = cls.class_map[class_label]
+        individual_name: str = f"{image_name}_{x}_{y}_{individual_class[1]}"
+        individual: object = individual_class[0](individual_name)
         individual.hasX = x
         individual.hasY = y
         individual.hasPercentage = float(percentage)
@@ -88,8 +120,8 @@ class IndividualGenerator():
         onto.save()
 
     @classmethod
-    def create_image_individual(cls, image_uri, longitude, lantitude):
-        image = Image(os.path.basename(image_uri))
+    def create_image_individual(cls, image_uri: str, longitude: float, lantitude:float) -> None:
+        image: Image = Image(os.path.basename(image_uri))
         image.hasImageUri = image_uri
         image.hasLongitude = longitude
         image.hasLatitude = lantitude
